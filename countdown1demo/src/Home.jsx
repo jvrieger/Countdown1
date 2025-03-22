24147243a9d3c4cc23997ef5e2c943bd28c73139
jrieger <juliavrieger@gmail.com>
2025-03-22 15:54:00 -0400

    EOD Deliverables

---
+++ countdown1demo/src/Home.jsx
@@ -0,0 +1,17 @@
+import "./styles/Home.css"
+
+function Home() {
+
+  return (
+    <>
+      <h1 className="title">My name is Julia Rieger</h1>
+      <p>I am currently a student at Bryn Mawr College, and will graduate in May 2025!</p>
+      <ul>
+        <li className="list-item">Girls Who Code</li>
+        <li className="list-item">Algorithms TA</li>
+      </ul>
+    </>
+  );
+}
+
+export default Home;
